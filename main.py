9daff8464fa702adb45083cc40f720828947c93a
Subhashree Priyadarshini <160435792+subhuchan@users.noreply.github.com>
2025-12-09 05:52:28 +0530

    Fix formatting issues in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
-st.title("AtliQ T Shirts: Database Q&A 👕")
+st.title("AtliQ T Shirts: Database Q&A")
 
 question = st.text_input("Question: ")
 
@@ -15,3 +15,4 @@ if question:
         st.write(response.get("result", response))
     else:
         st.write(response)
+
